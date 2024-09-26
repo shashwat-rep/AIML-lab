fce1267700a0bd6b621161d59c465f9c2dbc4a77
Shashwat Kashyap <Shashwat.124830@stu.upes.ac.in>
2024-09-26 09:10:03 +0530

    Update Experiment 5.py

--- Experiment 5.py
+++ Experiment 5.py
@@ -1,1 +1,7 @@
+# Experiment - 5 : Using Python language do the exploratory data analysis of dataset imported in the lab 4.
 
+import pandas as pd
+from google.colab import files
+upload=files.upload()
+import io
+df = pd.read_csv(io.BytesIO(upload['sample.csv']))
